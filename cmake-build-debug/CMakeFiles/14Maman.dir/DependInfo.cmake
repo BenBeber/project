c29903eb4f8af334fd71fa2d124b566f301d0bf9
Ben <ben.beberashvili@gmail.com>
2020-04-06 00:07:43 +0300

    The CMAKE FILE

---
+++ cmake-build-debug/CMakeFiles/14Maman.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/cygdrive/c/Users/Ben/CLionProjects/14Maman/src/main.c" "/cygdrive/c/Users/Ben/CLionProjects/14Maman/cmake-build-debug/CMakeFiles/14Maman.dir/src/main.c.o"
+  "/cygdrive/c/Users/Ben/CLionProjects/14Maman/src/modules/Analyzer.c" "/cygdrive/c/Users/Ben/CLionProjects/14Maman/cmake-build-debug/CMakeFiles/14Maman.dir/src/modules/Analyzer.c.o"
+  "/cygdrive/c/Users/Ben/CLionProjects/14Maman/src/modules/CodeSegement.c" "/cygdrive/c/Users/Ben/CLionProjects/14Maman/cmake-build-debug/CMakeFiles/14Maman.dir/src/modules/CodeSegement.c.o"
+  "/cygdrive/c/Users/Ben/CLionProjects/14Maman/src/modules/CommandBuilder.c" "/cygdrive/c/Users/Ben/CLionProjects/14Maman/cmake-build-debug/CMakeFiles/14Maman.dir/src/modules/CommandBuilder.c.o"
+  "/cygdrive/c/Users/Ben/CLionProjects/14Maman/src/modules/CommandsIndex.c" "/cygdrive/c/Users/Ben/CLionProjects/14Maman/cmake-build-debug/CMakeFiles/14Maman.dir/src/modules/CommandsIndex.c.o"
+  "/cygdrive/c/Users/Ben/CLionProjects/14Maman/src/modules/DataSegment.c" "/cygdrive/c/Users/Ben/CLionProjects/14Maman/cmake-build-debug/CMakeFiles/14Maman.dir/src/modules/DataSegment.c.o"
+  "/cygdrive/c/Users/Ben/CLionProjects/14Maman/src/modules/Opearnds.c" "/cygdrive/c/Users/Ben/CLionProjects/14Maman/cmake-build-debug/CMakeFiles/14Maman.dir/src/modules/Opearnds.c.o"
+  "/cygdrive/c/Users/Ben/CLionProjects/14Maman/src/modules/SymbolsTable.c" "/cygdrive/c/Users/Ben/CLionProjects/14Maman/cmake-build-debug/CMakeFiles/14Maman.dir/src/modules/SymbolsTable.c.o"
+  "/cygdrive/c/Users/Ben/CLionProjects/14Maman/src/phase1.c" "/cygdrive/c/Users/Ben/CLionProjects/14Maman/cmake-build-debug/CMakeFiles/14Maman.dir/src/phase1.c.o"
+  "/cygdrive/c/Users/Ben/CLionProjects/14Maman/src/phase2.c" "/cygdrive/c/Users/Ben/CLionProjects/14Maman/cmake-build-debug/CMakeFiles/14Maman.dir/src/phase2.c.o"
+  "/cygdrive/c/Users/Ben/CLionProjects/14Maman/src/utils/Binary.c" "/cygdrive/c/Users/Ben/CLionProjects/14Maman/cmake-build-debug/CMakeFiles/14Maman.dir/src/utils/Binary.c.o"
+  "/cygdrive/c/Users/Ben/CLionProjects/14Maman/src/utils/Error.c" "/cygdrive/c/Users/Ben/CLionProjects/14Maman/cmake-build-debug/CMakeFiles/14Maman.dir/src/utils/Error.c.o"
+  "/cygdrive/c/Users/Ben/CLionProjects/14Maman/src/utils/FileReader.c" "/cygdrive/c/Users/Ben/CLionProjects/14Maman/cmake-build-debug/CMakeFiles/14Maman.dir/src/utils/FileReader.c.o"
+  "/cygdrive/c/Users/Ben/CLionProjects/14Maman/src/utils/FileWriter.c" "/cygdrive/c/Users/Ben/CLionProjects/14Maman/cmake-build-debug/CMakeFiles/14Maman.dir/src/utils/FileWriter.c.o"
+  "/cygdrive/c/Users/Ben/CLionProjects/14Maman/src/utils/List.c" "/cygdrive/c/Users/Ben/CLionProjects/14Maman/cmake-build-debug/CMakeFiles/14Maman.dir/src/utils/List.c.o"
+  "/cygdrive/c/Users/Ben/CLionProjects/14Maman/src/utils/TextUtils.c" "/cygdrive/c/Users/Ben/CLionProjects/14Maman/cmake-build-debug/CMakeFiles/14Maman.dir/src/utils/TextUtils.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/14Maman.dir/cmake_clean.cmake
@@ -0,0 +1,26 @@
+file(REMOVE_RECURSE
+  "14Maman.exe"
+  "14Maman.pdb"
+  "CMakeFiles/14Maman.dir/src/main.c.o"
+  "CMakeFiles/14Maman.dir/src/modules/Analyzer.c.o"
+  "CMakeFiles/14Maman.dir/src/modules/CodeSegement.c.o"
+  "CMakeFiles/14Maman.dir/src/modules/CommandBuilder.c.o"
+  "CMakeFiles/14Maman.dir/src/modules/CommandsIndex.c.o"
+  "CMakeFiles/14Maman.dir/src/modules/DataSegment.c.o"
+  "CMakeFiles/14Maman.dir/src/modules/Opearnds.c.o"
+  "CMakeFiles/14Maman.dir/src/modules/SymbolsTable.c.o"
+  "CMakeFiles/14Maman.dir/src/phase1.c.o"
+  "CMakeFiles/14Maman.dir/src/phase2.c.o"
+  "CMakeFiles/14Maman.dir/src/utils/Binary.c.o"
+  "CMakeFiles/14Maman.dir/src/utils/Error.c.o"
+  "CMakeFiles/14Maman.dir/src/utils/FileReader.c.o"
+  "CMakeFiles/14Maman.dir/src/utils/FileWriter.c.o"
+  "CMakeFiles/14Maman.dir/src/utils/List.c.o"
+  "CMakeFiles/14Maman.dir/src/utils/TextUtils.c.o"
+  "lib14Maman.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/14Maman.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
